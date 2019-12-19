61aa937f96533da39cc21db772bb97d617b25f49
Marcus Gardiner <marcus.gardiner@softwire.com>
2019-12-19 19:56:01 +0000

    Day1 Complete

---
+++ Day1/AdventOfCodeConsoleApp/AdventOfCodeConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCodeConsoleApp
+{
+    internal class Program
+    {
+        private static readonly string textFile = @"C:\Work\PizzaAndProgramming\AdventOfCode\Day1\puzzleInput.txt";
+
+        public static void Main(string[] args)
+        {
+//take its mass, divide by three, round down, and subtract 2.
+            string[] lines = File.ReadAllLines(textFile);
+            double sumOfLines = 0;
+            foreach (var line in lines)
+            {
+                var number = double.Parse(line);
+                CalculateFuel(number);
+            }
+            Console.WriteLine(sumOfLines);
+
+            void CalculateFuel(double number)
+            {
+                var roundedDownNumber = Math.Floor(number / 3);
+                if (roundedDownNumber <= 2)
+                {
+                    return;
+                }
+                var finalFuelNumber = roundedDownNumber - 2;
+                sumOfLines += finalFuelNumber;
+                CalculateFuel(finalFuelNumber);
+            }
+        }
+        
+    }
+}
